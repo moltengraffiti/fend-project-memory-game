2a2526e323603deb3d6919f74db103b84152f5b9
Sam Reilly <samreilly@gmail.com>
2019-01-20 16:39:51 +1100

    Updated remaining files

--- README.md
+++ README.md
@@ -1,20 +1,38 @@
+<link rel="stylesheet prefetch" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
 # Memory Game Project
 
 ## Table of Contents
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+- [Memory Game Project](#memory-game-project)
+  - [Table of Contents](#table-of-contents)
+  - [About the Game](#about-the-game)
+  - [Instructions](#instructions)
+  - [Dependancies](#dependancies)
+  - [Authors](#authors)
+  - [License](#license)
+
+## About the Game
+The Memory Game is a simple game for testing a user's memory. By clicking cards in the deck to reaveal icons, users must succesfuly match card pairs to win the game. 
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+1. Load the **index.html** file from the Project 2 folder.
+2. Click on cards in the deck to find matched pairs.
+3. A timer starts at the first card click and ends when the game is won.
+4. A star based <i class="fa fa-star"></i> rating system is included and counts down from one to five stars.
+5. Use the Refresh icon <i class="fa fa-repeat" aria-hidden="true"></i> to reset the game at any time.
+6. Upon winning the game, a modal is displayed showing the final player stats.
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Dependancies
+External Dependencies, used in the index.html file to add styling, icons and font.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+-Bootstrap [Bootstrap]("https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css")
+-Google Fonts [Font]("https://fonts.googleapis.com/css?family=Coda")
+-Font Awesome [FontAwesome]("https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css")
 
-## Contributing
+## Authors
+Source code provided as part of Project 2- Memory game in the FEND nanodegree by Udacity. Interactivity using javascript and some styling added.
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- js/app.js
+++ js/app.js
@@ -1,14 +1,234 @@
-/*
- * Create a list that holds all of your cards
- */
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+let cardList = [];
+let clickCount = 0;
+let moveCount = document.querySelector('.moves');
+let startOver = document.querySelector('.restart');
+let modalCancel=document.querySelector('.modal-exit');
+let modalReplay=document.querySelector('.modal-replay');
+const deck = document.querySelector('.deck');
+let cardDeck = document.querySelectorAll('.deck li');
+let cardArray = Array.from(cardDeck);
+let clockTime = document.querySelector('.time');
+let finalTime; 
+let playerRating = document.querySelectorAll('.stars li');
+const theModal=document.querySelector('#winModal');
+const timeStat=document.querySelector('.modalTime');
+const playerStat=document.querySelector('.modalRating');
+const moveStat=document.querySelector('.modalMoves');
+const maxMatched = 8;
+let clockOn = false;
+let matched = 0;
+let timer = 0;
+let minutes = 0;
+let seconds = 0;
+let starCount=playerRating.length;
+let clockFunction;
+let shuffledCards;
+
+shuffleDeck();
+
+startOver.addEventListener('click', function (click) {
+    const resetTarget = click.target;
+    console.log('Restart button clicked')
+    resetGame();
+})
+
+modalCancel.addEventListener('click', function(){
+    toggleModal();
+    resetTimer();
+})
+modalReplay.addEventListener('click', function(){
+    toggleModal();
+    resetGame();
+})
+
+
+deck.addEventListener('click', function (event) {
+    const clickTarget = event.target;
+    if (clickTarget.classList.contains('card') && cardList.length < 2) {
+        //Replace with function isValid
+        if (!clockOn) {
+            startTimer();
+            clockOn = true;
+           // theModal.classList.toggle('modal');
+        }
+
+        if (!clickTarget.classList.contains('open') && !clickTarget.classList.contains('show') && !clickTarget.classList.contains('match')) {
+            console.log('Card clicked');
+            showCard(clickTarget);
+            addCard(clickTarget);
+
+            console.log('CardList is ' + cardList.length)
+        }
+        if (cardList.length === 2) {
+            clickCounter();
+            checkMatch(cardList);
+        }
+    }
+})
+
+function showCard(clickTarget) {
+    clickTarget.classList.toggle('open');
+    clickTarget.classList.toggle('show');
+}
+
+function addCard(clickTarget) {
+    cardList.push(clickTarget);
+}
+
+function matchCard(clickTarget) {
+    clickTarget.classList.toggle('match');
+}
+function startTimer() {
+    clockFunction = setInterval(function(){
+        timer++;
+        displayTime();
+    }, 1000);
+    
+}
+function displayTime(){
+    minutes = Math.floor(timer / 60);
+    seconds = timer % 60;
+    console.log('Elapsed time is now' + minutes + seconds);
+    if(seconds<10){
+    clockTime.innerHTML = minutes+':0'+seconds;
+    }
+    else{
+        clockTime.innerHTML = minutes+':'+seconds;
+    }
+    
+}
+
+function clickCounter() {
+    clickCount++;
+    console.log('Clickcount is now ' + clickCount);
+    if (clickCount % 8 === 0) {
+       
+        checkRating();
+    }
+    moveCount.innerHTML = clickCount;
+}
+function countMatch() {
+    matched++;
+    console.log('Pairs matched is now ' + matched);
+    //moveCount.innerHTML = clickCount;
+    if (matched === maxMatched) {
+        console.log('The Game has been won!');
+        gameOverManGameOver();
+    }
+}
+function checkMatch(cardList) {
+    if (cardList[0].firstElementChild.className === cardList[1].firstElementChild.className) {
+        console.log('A Match')
+        //Is there a better way to call these functions?
+        matchCard(cardList[0]);
+        matchCard(cardList[1]);
+        showCard(cardList[0]);
+        showCard(cardList[1]);
+        cardList.length = 0;
+        countMatch();
+    }
+    else {
+        console.log('No Match')
+        setTimeout(function () {
+            showCard(cardList[0]);
+            showCard(cardList[1]);
+            clearArray(cardList);
+        }, 1200);
+    }
+}
+
+function checkRating() {
+    console.log('Rating is being checked');
+    for (star of playerRating) {
+        if(starCount>1){
+        if (star.style.display != 'none') {
+            star.style.display = 'none';
+            starCount--;
+            break;
+            
+        }
+    }
+
+        /*Couldn't get the below to work, added a hidden class in the css...
+        if(!star.classList.contains('hidden')){
+            star.classList.add('hidden');
+        }
+            */
+    }
+}
+
+function resetGame() {
+    clearArray(cardList);
+    resetCounter();
+    resetMatch();
+    resetTimer();
+    resetStars();
+    resetCards();
+    shuffleDeck();
+    
+}
+
+function clearArray(cardList) {
+    cardList.length = 0;
+}
+
+function resetMatch() {
+    matched = 0;
+    console.log('Matchcount has been reset, is now ' + matched);
+}
+
+function resetCounter() {
+    clickCount = 0;
+    console.log('Clickcount has been reset, is now ' + clickCount);
+    moveCount.innerHTML = clickCount;
+}
+
+function resetTimer() {
+    clockOn = false;
+    clearInterval(clockFunction);
+    timer = 0;
+    console.log('Clock has been reset, is now ' + timer);
+}
+
+function resetStars() {
+    for (star of playerRating) {
+        star.style.display = 'inline';
+    }
+}
+
+function resetCards() {
+    for (card of cardDeck) {
+        card.className = 'card';
+    }
+}
+
+function gameOverManGameOver() {
+    showStats();
+    toggleModal();
+      
+}
+function toggleModal(){
+    theModal.classList.toggle('modal');
+}
+
+function showStats(){
+    finalTime = clockTime.innerHTML;
+    timeStat.innerHTML='Total Time: ' + finalTime;
+    moveStat.innerHTML='Total Moves: ' + clickCount;
+    playerStat.innerHTML='Player Rating: '+ starCount;
+    resetTimer();
+    
+    
+}
+
+function shuffleDeck() {
+    shuffledCards = shuffle(cardArray);
+    for (card of shuffledCards) {
+        deck.appendChild(card);
+    }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -27,6 +247,33 @@ function shuffle(array) {
 
 
 /*
+Core:
+If a card is clicked, set to to open show, unless 1) Its open already 2) Has been matched already
+add to an array and determine if a0 = a1, if it does set both to matched and clear the array,
+    else remove open show ( timeout)
+    Functions:
+    onclick()
+    showcard()
+    matchcard()
+    hidecard() - No
+    clearArray()? - yes
+    counter()
+    calcScore()
+
+Start/Reset Game:
+Need to ramdomly arrange cards in the grid, default hidden
+Initilise( shuffle()
+Counter = 0
+Stars=3
+Timer = 0
+)
+
+User Info:
+Reset at any time - Initialise()
+Count Moves
+Show Time
+
+/*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
@@ -36,3 +283,32 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Create a list that holds all of your cards
+ */
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+
+
+
